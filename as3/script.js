daa1ca0a93ab34695a7b333dfdbde9d902670efb
hsutingyun <149577749+hsutingyun@users.noreply.github.com>
2024-10-20 19:34:16 +0800

    reverse

--- as3/script.js
+++ as3/script.js
@@ -2,12 +2,12 @@ window.addEventListener('scroll', function() {
   let scrollPosition = window.scrollY;
   let backgroundSection = document.querySelector('.background-section');
 
-  backgroundSection.style.backgroundPosition = `center ${scrollPosition * 0.2}px`;
+  backgroundSection.style.backgroundPosition = `center ${scrollPosition * 0.1}px`;
 
-  if (scrollPosition > 850) {
+  if (scrollPosition > 550) {
       backgroundSection.classList.add('sticky'); 
       backgroundSection.style.backgroundImage = "url('Webaw3_picture/p25.PNG')";
-      backgroundSection.style.backgroundPosition = "center top"; 
+      backgroundSection.style.backgroundPosition = "center top 50%"; 
   } else {
       backgroundSection.classList.remove('sticky'); 
       backgroundSection.style.backgroundImage = "url('Webaw3_picture/p25.PNG')";
