d9b87ed4296064c3ca024a09d309bbbbe69b5c77
hsutingyun <149577749+hsutingyun@users.noreply.github.com>
2025-06-22 14:38:19 +0800

    update

--- ntujour2024/script.js
+++ ntujour2024/script.js
@@ -761,6 +761,40 @@ document.addEventListener('DOMContentLoaded', function () {
     imageObserver.observe(section5);
   });
 
+  // 時間軸
+  (function () {
+    "use strict";
+
+    // define variables
+    var items = document.querySelectorAll(".timeline li");
+
+    // check if an element is in viewport
+    // http://stackoverflow.com/questions/123999/how-to-tell-if-a-dom-element-is-visible-in-the-current-viewport
+    function isElementInViewport(el) {
+      var rect = el.getBoundingClientRect();
+      return (
+        rect.top >= 0 &&
+        rect.left >= 0 &&
+        rect.bottom <=
+        (window.innerHeight || document.documentElement.clientHeight) &&
+        rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+      );
+    }
+
+    function callbackFunc() {
+      for (var i = 0; i < items.length; i++) {
+        if (isElementInViewport(items[i])) {
+          items[i].classList.add("in-view");
+        }
+      }
+    }
+
+    // listen for events
+    window.addEventListener("load", callbackFunc);
+    window.addEventListener("resize", callbackFunc);
+    window.addEventListener("scroll", callbackFunc);
+  })();
+
   // Video Background and Content Sections with ScrollTrigger
   // 處理文字動畫
   window.addEventListener('load', () => {
@@ -790,12 +824,14 @@ document.addEventListener('DOMContentLoaded', function () {
           }
         });
       }
+
       function playVideo(video) {
-          video.play().catch(error => {
-            console.log("Video play was prevented:", error);
-          });
-          video.classList.add('active');
+        video.play().catch(error => {
+          console.log("Video play was prevented:", error);
+        });
+        video.classList.add('active');
       }
+
       function pauseVideo(video) {
         video.pause();
         video.classList.remove('active');
